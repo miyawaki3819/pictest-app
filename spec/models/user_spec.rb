62bda42de90f30b681bf6fdf45e891669d2da0b2
miyawaki3819 <soler.creus@gmail.com>
2025-07-29 14:09:52 +0900

    テスト例の追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe User, type: :model do
   end
   describe 'ユーザー新規登録' do
     # ユーザー新規登録についてのテストコードを記述します  
+    it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+    end
     it 'nicknameが空では登録できない' do
       # nicknameが空では登録できないテストコードを記述します
       user.nickname = ''  # nicknameの値を空にする
@@ -17,6 +19,20 @@ RSpec.describe User, type: :model do
       user.email = ''  # emailの値を空にする
       user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
+    end
+    it 'nicknameが7文字以上では登録できない' do
+    end
+    it '重複したemailが存在する場合は登録できない' do
+    end
+    it 'emailは@を含まないと登録できない' do
+    end
+    it 'passwordが5文字以下では登録できない' do
+    end
+    it 'passwordが129文字以上では登録できない' do
     end
   end
 end
